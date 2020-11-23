294cd97826463a53b6ae58c4c890006a599a27f2
OlehStrong <yavtushenkooleh1@gmail.com>
2020-11-23 17:21:45 +0200

    first commit

---
+++ minimalmaximal.cpp
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+#include<vector>
+using namespace std;
+int main()
+{
+	int a, min, max;
+	min = 0;
+	max = 0;
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	scanf("%d\n", &a);
+
+	vector <int>arr(a, 0);
+	for (int i = 0; i < arr.size(); i++) {
+		scanf("%d", &arr[i]);
+	}
+
+	for (int i = 0; i < arr.size(); i++) {
+
+		if (arr[i] < min) min = arr[i];
+	}
+	for (int i = 0; i < arr.size(); i++) {
+
+		if (arr[i] > max) max = arr[i];
+	}
+	printf("%d %d", min, max);
+}
---
+++ twotriangle.cpp
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+#include<vector>
+using namespace std;
+int main()
+{
+	int a;
+	
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	scanf("%d\n", &a);
+
+	vector <int>arr(a);
+	vector <int>arr1(a);
+	for (int i = 0; i < arr.size(); i++) {
+		scanf("%d%d\n", &arr[i],&arr1[i]);	
+	}
+	int min= arr[0] * arr1[0];
+	for (int i = 0; i < arr.size(); i++) {
+		if (arr[i] * arr1[i] <= min)
+			min = arr[i] * arr1[i];
+
+	}
+	printf("%d", min);
+}
